6aa60651d3d31cc2f54b2271bd9bd12921d36e9d
Ryuji Oda <ryuji.oda@gmail.com>
2022-01-30 00:35:14 +0900

    add: hilt module for interface dependency

--- app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
+++ app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -1,4 +1,29 @@
 package com.example.android.hilt.di
 
-class DatabaseModule {
+import android.content.Context
+import androidx.room.Room
+import com.example.android.hilt.data.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "logging.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideLogDao(database: AppDatabase) = database.logDao()
 }
