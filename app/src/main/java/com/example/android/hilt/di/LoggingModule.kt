4121d7a8f07d8cd2b93224a7e865c22a5e3f1f9a
ryujioda <ryuji.oda@gmail.com>
2022-02-06 17:00:35 +0900

    add: selectable injection of concrete class on each fragment

--- app/src/main/java/com/example/android/hilt/di/LoggingModule.kt
+++ app/src/main/java/com/example/android/hilt/di/LoggingModule.kt
@@ -6,23 +6,33 @@ import com.example.android.hilt.data.LoggerLocalDataSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Qualifier
 import javax.inject.Singleton
+
+@Qualifier
+annotation class LoggerLocal
+
+@Qualifier
+annotation class LoggerInMemory
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LoggingLocalDataModule {
 
+    @LoggerLocal
     @Singleton
     @Binds
     abstract fun bindLoggerLocalDataSource(impl: LoggerLocalDataSource): LoggerDataSource
 }
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 abstract class LoggingInMemoryDataModule {
 
+    @LoggerInMemory
     @ActivityScoped
     @Binds
     abstract fun bindLoggerInMemoryDataSource(impl: LoggerInMemoryDataSource): LoggerDataSource
--- app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -24,7 +24,8 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.android.hilt.R
 import com.example.android.hilt.data.LoggerDataSource
-import com.example.android.hilt.data.LoggerLocalDataSource
+import com.example.android.hilt.di.LoggerInMemory
+import com.example.android.hilt.di.LoggerLocal
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,13 +35,12 @@ import javax.inject.Inject
  * Fragment that displays buttons whose interactions are recorded.
  */
 @AndroidEntryPoint
-class ButtonsFragment @Inject constructor(
-    var logger: LoggerDataSource,
-    var navigator: AppNavigator
-) : Fragment() {
+class ButtonsFragment : Fragment() {
 
-//    @Inject lateinit var logger: LoggerDataSource
-//    @Inject lateinit var navigator: AppNavigator
+//    @LoggerInMemory
+    @LoggerLocal
+    @Inject lateinit var logger: LoggerDataSource
+    @Inject lateinit var navigator: AppNavigator
 
     override fun onCreateView(
         inflater: LayoutInflater,
